8ae889ec349244a427ec3af0f57b028c28cf6318
nima <ni_moradi@yahoo.com>
2018-07-12 21:57:18 +0430

    playing around

---
+++ app/src/main/java/com/nima/powerprototype/components/customEditText.java
@@ -0,0 +1,20 @@
+package com.nima.powerprototype.components;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Paint.Style;
+import android.util.AttributeSet;
+import android.view.View;
+/**
+ * Created by nima on 7/12/2018.
+ */
+
+public class customEditText extends View {
+
+    public customEditText(Context context) {
+        super(context);
+    }
+
+}
